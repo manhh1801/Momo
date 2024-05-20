dafab171efb61835fd2ae320e6c781d9488d25a5
manhnguyen1801 <manhforbusiness@gmail.com>
2024-05-20 10:43:37 +0700

    FINAL

--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -8,7 +8,7 @@ export default function SignIn() {
             <div className="flex flex-col space-y-2 text-center">
                 <Icons.logo className="mx-auto h-6 w-6"/>
                 <h1 className="text-2xl font-semibold tracking-tight">Welcome back</h1>
-                <p className="text-sm max-w-xs mx-auto"> By continuing, you are setting up a Mikan account and agree to our User Agreement and Privacy Policy.</p>
+                <p className="text-sm max-w-xs mx-auto"> By continuing, you are setting up a Momo account and agree to our User Agreement and Privacy Policy.</p>
                 <UserAuthForm/>
                 <p className="px-8 text-center text-sm text-zinc-700">
                     New to Momo?{' '}
